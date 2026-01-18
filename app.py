8748bea1c78785696203af8b5a53d544c0605091
ManikandanRamesh <manibe312@gmail.com>
2026-01-18 09:17:51 +0530

    Updated home page

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello! Flask app is running."
+    return "Hello! everyone This is version 2 of my deployed app"
 
 if __name__=="__main__":
     app.run(debug=True)
